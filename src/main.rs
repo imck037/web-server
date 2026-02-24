07c3348548c5a8720f8ea08479bddf24168fa531
imck037 <chandradhar.2003@gmail.com>
2026-02-25 02:18:11 +0530

    code refactoring

--- src/main.rs
+++ src/main.rs
@@ -18,17 +18,13 @@ fn main() {
 fn handle_client(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
-    if request_line == "GET / HTTP/1.1" {
-        let contents = fs::read_to_string("index.html").unwrap();
-        let lenth = contents.len();
-        let status_line = "HTTP/1.1 200 OK";
-        let response = format!("{status_line}\r\nContent-Lenth: {lenth}\r\n\r\n{contents}");
-        stream.write_all(response.as_bytes()).unwrap();
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "index.html")
     } else {
-        let contents = fs::read_to_string("404.html").unwrap();
-        let lenth = contents.len();
-        let status_line = "HTTP/1.1 404 NOT FOUND";
-        let response = format!("{status_line}\r\nContent-Lenth: {lenth}\r\n\r\n{contents}");
-        stream.write_all(response.as_bytes()).unwrap();
-    }
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+    let contents = fs::read_to_string(filename).unwrap();
+    let lenth = contents.len();
+    let response = format!("{status_line}\r\nContent-Lenth: {lenth}\r\n\r\n{contents}");
+    stream.write_all(response.as_bytes()).unwrap();
 }
