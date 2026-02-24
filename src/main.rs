597547741bbc960d92118384cacbf43353661ee4
imck037 <chandradhar.2003@gmail.com>
2026-02-25 02:05:37 +0530

    404 error status handeled.....

--- src/main.rs
+++ src/main.rs
@@ -17,16 +17,18 @@ fn main() {
 #[allow(unused)]
 fn handle_client(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
-
-    println!("{http_request:#?}");
-    let contents = fs::read_to_string("index.html").unwrap();
-    let lenth = contents.len();
-    let status_line = "http/1.1 200 ok";
-    let response = format!("{status_line}\r\nContent-Lentg: {lenth}\r\n\r\n{contents}");
-    stream.write_all(response.as_bytes()).unwrap();
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    if request_line == "GET / HTTP/1.1" {
+        let contents = fs::read_to_string("index.html").unwrap();
+        let lenth = contents.len();
+        let status_line = "HTTP/1.1 200 OK";
+        let response = format!("{status_line}\r\nContent-Lenth: {lenth}\r\n\r\n{contents}");
+        stream.write_all(response.as_bytes()).unwrap();
+    } else {
+        let contents = fs::read_to_string("404.html").unwrap();
+        let lenth = contents.len();
+        let status_line = "HTTP/1.1 404 NOT FOUND";
+        let response = format!("{status_line}\r\nContent-Lenth: {lenth}\r\n\r\n{contents}");
+        stream.write_all(response.as_bytes()).unwrap();
+    }
 }
